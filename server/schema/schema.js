66981613495b49a5486fef7ed3a26b4cb31974dd
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2023-11-05 02:20:01 +0530

    update project mutation added

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -140,6 +140,38 @@ const mutation = new GraphQLObjectType({
 				return Project.findByIdAndRemove(args.id);
 			},
 		},
+		updateProject: {
+			type: ProjectType,
+			args: {
+				id: { type: GraphQLNonNull(GraphQLString) },
+				name: { type: GraphQLString },
+				description: { type: GraphQLString },
+				status: {
+					type: new GraphQLEnumType({
+						name: "ProjectStatusUpdate",
+						values: {
+							new: { value: "Not Started" },
+							progress: { value: "In Progress" },
+							completed: { value: "Completed" },
+						},
+					}),
+					defaultValue: "Not Started",
+				},
+			},
+			resolve(parent, args) {
+				return Project.findByIdAndUpdate(
+					args.id,
+					{
+						$set: {
+							name: args.name,
+							description: args.description,
+							status: args.status,
+						},
+					},
+					{ new: true }
+				);
+			},
+		},
 	},
 });
 
