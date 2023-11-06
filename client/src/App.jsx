9f6b49804fe0e8f169ded592610835461956e3a4
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2023-11-06 18:35:23 +0530

    Client side of project started

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h1>
+          Project Management App
+        </h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
