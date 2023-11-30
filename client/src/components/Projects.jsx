7cf26d1e0e753c3199fa916701a720d837d77899
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2023-11-30 22:00:33 +0530

    project section added

---
+++ client/src/components/Projects.jsx
@@ -0,0 +1,24 @@
+import  Spinner  from "./Spinner"
+import { useQuery } from "@apollo/client"
+import { GET_PROJECTS } from "../queries/ProjectQueries"
+import ProjectCard from "./ProjectCard"
+
+function Projects() {
+    const { loading, error, data } = useQuery(GET_PROJECTS);
+    if (loading) return <Spinner />;
+    if (error) return <div>Something went wrong!</div>;
+    return (
+        <>
+            {
+            data.projects.length > 0 ? (
+                <div className="row">
+                    {data.projects.map((project) => (<ProjectCard key={project.id} project={project}/>))}
+                </div>
+            ) : 
+            (<p>No Projects </p>) 
+        }
+        </>
+    )
+}
+
+export default Projects
